42f9258adb94aa4e00a3170c94c871eaf1c82ef7
FelixTheC <felixeisenmenger@gmx.net>
2020-10-10 22:59:14 +0200

    [UPD] mysql set strict mode

--- beak_emailer_list/settings.py
+++ beak_emailer_list/settings.py
@@ -119,14 +119,10 @@ except ImportError:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
             'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
-        },
-        'TEST': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('DB_TEST'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASSWORD'),
             'HOST': os.environ.get('DB_HOST'),
