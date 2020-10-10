c2e5409e8ef354633d53c228c3fc1837c8463f02
FelixTheC <felixeisenmenger@gmx.net>
2020-10-10 23:02:46 +0200

    [UPD] load env variables with dotenv

--- beak_emailer_list/settings.py
+++ beak_emailer_list/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# load environment variables
+load_dotenv(Path(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
