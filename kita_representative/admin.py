98111a3d3128aadd2c147978350fd95579aae839
FelixTheC <felixeisenmenger@gmx.net>
2020-10-17 19:31:54 +0200

    [CRE] kitarepresenatives admin

--- kita_representative/admin.py
+++ kita_representative/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from kita_representative.models import KitaRepresentative
+
+
+class KitaRepresentativeAdminModel(admin.ModelAdmin):
+    list_display = ('name', 'email', 'kita')
+    list_filter = ('name', 'kita')
+
+
+admin.site.register(KitaRepresentative, KitaRepresentativeAdminModel)
+
