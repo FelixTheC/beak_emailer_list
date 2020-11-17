b9886649d7e9197107f1530cc7a1d625b3ba195c
FelixTheC <felixeisenmenger@gmx.net>
2020-11-17 22:46:20 +0100

    [FIX] whitenoise not required

--- beak_emailer_list/settings.py
+++ beak_emailer_list/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -165,7 +164,6 @@ except ImportError:
     DEBUG = False
 
     ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-
 
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
