c9d6fdf25b5b6eb57fab9e10bebbe411f533055a
felix <FTheC87@8e86bfb0e784>
2021-09-05 08:05:48 +0000

    fast changes on production

--- additonal_utils/models.py
+++ additonal_utils/models.py
@@ -4,18 +4,21 @@
 @created: 01.05.21
 @author: felix
 """
+import uuid
+
 from django.db import models
+from django.db.models import BigAutoField
 
 
 class BigPrimaryKeyModel(models.Model):
-    id = models.BigIntegerField(primary_key=True, db_index=True)
+    # id = models.BigIntegerField(primary_key=True, db_index=True, default=BigAutoField)
 
     class Meta:
         abstract = True
 
 
 class UUIDPrimaryKeyModel(models.Model):
-    id = models.UUIDField(primary_key=True, db_index=True)
+    id = models.UUIDField(primary_key=True, db_index=True, null=True, blank=True, default=uuid.uuid4)
 
     class Meta:
         abstract = True
--- kita/admin.py
+++ kita/admin.py
@@ -1,11 +1,25 @@
 from urllib.parse import urlencode
 
 from django.contrib import admin
+from django.contrib import messages
+from django.db.models import QuerySet
 from django.urls import reverse
 from django.utils.html import format_html
 
 from kita.models import Kita
 from kita_representative.models import KitaRepresentative
+import pandas as pd
+from pathlib import Path
+
+
+def kitas_to_csv(modeladmin, request, queryset: QuerySet):
+    df = pd.DataFrame.from_records(Kita.objects.all().values())
+    file_path = Path(__file__).parent / Path("kita.csv")
+    df.to_csv(str(file_path.absolute()))
+    messages.info(request, f'#{file_path}.')
+    return
+
+kitas_to_csv.short_description = 'Kitas to csv'
 
 
 @admin.register(Kita)
@@ -13,7 +27,9 @@ class KitaAdminModel(admin.ModelAdmin):
     list_display = ('name', 'street_name', 'email', 'created_at', 'view_kita_representatives')
     list_filter = ('name',)
     search_fields = ('name__startswith', 'name__icontains',
-                     'street_name__startswith', 'street_name__icontains')
+                     'street_name__startswith', 'street_name__icontains',
+                     'email__startswith', 'email__icontains')
+    actions = (kitas_to_csv, )
 
     def view_kita_representatives(self, obj: Kita):
         count = KitaRepresentative.objects.filter(kita=obj).count()
